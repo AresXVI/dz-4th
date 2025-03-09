0b422e51a45137874604e9ada3fd7c00d94f4f2d
AresXVI <nuriksatymbaev7@gmail.com>
2025-03-09 19:33:22 +0600

    Update AboutPage.jsx

--- dz3/src/pages/mainPage/AboutPage.jsx
+++ dz3/src/pages/mainPage/AboutPage.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import Description from '../../components/description/Description'
 import style from './aboutPage.module.css'
 function AboutPage() {
+  const data = {title: 'Title 2', description: 'description 2'}
   return (
     <div className={style.aboutPage}>
-      <Description data={{title: 'Title 2', description: 'description 2'}}/>
+      <Description data={data}/>
     </div>
   )
 }
