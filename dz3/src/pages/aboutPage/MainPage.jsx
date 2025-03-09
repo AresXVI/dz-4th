11a96e86ed14318815920d3c0e6b915ec37a5d38
AresXVI <nuriksatymbaev7@gmail.com>
2025-03-09 19:31:26 +0600

    Update MainPage.jsx

--- dz3/src/pages/aboutPage/MainPage.jsx
+++ dz3/src/pages/aboutPage/MainPage.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import Description from '../../components/description/Description'
 import style from './mainPage.module.css'
 function MainPage() {
+  const data = {title: 'Title', description: 'description'}
   return (
     <div className={style.mainPage}>
-      <Description data={{title: 'Title', description: 'description'}}/>
+      <Description data={data}/>
     </div>
   )
 }
